9903bb5e0e6115229991c8561b4927b2519eda60
DZ <1494977853@qq.com>
2021-10-11 09:52:15 +0800

    complete config

---
+++ opencv_code/opencv4/manuscript/main.cpp
@@ -0,0 +1,14 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char** argv)
+{
+	Mat A = Mat_<double>(3, 3);
+
+
+
+	return 0;
+}
